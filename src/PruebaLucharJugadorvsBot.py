d53f982e2ed931aeeba90011e328eef550dff23b
SergioPujol98 <sergiopujol.98@gmail.com>
2020-03-04 11:21:06 +0100

    Prueba

--- src/PruebaLucharJugadorvsBot.py
+++ src/PruebaLucharJugadorvsBot.py
@@ -1,6 +1,14 @@
 from src.Card import Card
+from src.Main import rootPlayer
+from src.MazoAleatorio import arrRandomCards
+from src.Player import Player
 
-def PlayerVsBot(Player, Enemy):
-    while (Player.life > 0 and Enemy.life > 0):
-        contador = 0;
-        while ()
+arrCardPlayer = arrRandomCards(rootPlayer)
+Jugador = Player('Sergio', arrCardPlayer);
+print(Player.life);
+print('Hola')
+
+def PlayerVsBot(arrCardPlayer, arrCardEnemy):
+    Jugador = Player('Sergio', arrCardPlayer);
+    print(Jugador.life);
+    print('Hola')
